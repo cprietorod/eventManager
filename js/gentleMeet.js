72a82e26f3c5dc44badc6dd1c76809e56d1c9398
carlos.prieto <carlos.prieto@eforcers.com>
2017-01-06 10:50:15 -0500

    added moment.js

--- js/gentleMeet.js
+++ js/gentleMeet.js
@@ -1,1 +1,23 @@
-alert("Hola mundo");
+"use strict";
+var $scope = (function(){
+  var $scope = {};
+
+  $scope.events = [];
+
+  $scope.init = function(){
+    //Configurar el dia actual
+    var currentDate = new Date();
+    //Configurar los proximos 4 eventos basados en la fecha actual
+    for(var i=0;i < 4; i++){
+      var
+    }
+  }
+
+  return $scope;
+})($scope);
+
+
+
+$(document).ready(function(){
+       $scope.init();
+});
